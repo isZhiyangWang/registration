d63ad87d797f95174d952ab295939aec1b640680
Zhiyang_Wang <87411831+JimmyXwtx@users.noreply.github.com>
2024-11-22 15:22:37 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/theo-test',
-  assetPrefix: '/theo-test',
 };
 export default nextConfig;
